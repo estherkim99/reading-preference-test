3aa293b1a98375ffd026886365d9678536459781
Yookyung Kim <estherkim@taggers.io>
2020-09-10 03:12:56 +0900

    Updates

---
+++ precache-manifest.38717e6c86d9aaf0dc22cefe353fa925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df325374e5657f6af6b5f3b4c596209d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a48de16fbd3784e755",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83b7305c64a0bcf4cd25",
+    "url": "/static/js/2.871487ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.871487ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a48de16fbd3784e755",
+    "url": "/static/js/main.f4772805.chunk.js"
+  },
+  {
+    "revision": "e877423bffc4093dc679",
+    "url": "/static/js/runtime-main.177d1ccd.js"
+  }
+]);
